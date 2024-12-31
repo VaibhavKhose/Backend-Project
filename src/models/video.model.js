c81e28732d132876ff0f4b158d0d9beac106d17e
VaibhavKhose <khosevaibhav9172@gmail.com>
2024-12-31 11:59:19 +0530

    User and video model with hooks and JWT

---
+++ src/models/video.model.js
@@ -0,0 +1,46 @@
+import mongoose, { Schema } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const videoSchema = new Schema(
+    {
+        videoFile:{
+            type:String, //cloudnary url
+            reqired :true
+        },
+         thumbnail:{
+            type:String,
+            reqired :true,
+         },
+        title:{
+            type:String,
+            reqired :true,
+         },
+         description:{
+            type:String,
+            reqired :true,
+         },
+         time:{
+            type:Number,
+            reqired :true,
+         },
+         view:{
+            type: Number,
+            default:0,
+         },
+         isPblished:{
+            type:Boolean,
+            default:true
+         },
+         owner:{
+            type:Schema.Types.ObjectId,
+            ref:"User"
+         }
+        
+    },
+    {
+        timestamps:true
+    }
+)
+videoSchema.plugin(mongooseAggregatePaginate)
+
+export const video = mongoose.model("video", videoSchema);
